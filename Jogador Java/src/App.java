63100f47ade2a61890e0b97cfcba25d7f2f49b5c
Hiran Kevin <117870293+HiranKDS@users.noreply.github.com>
2023-10-20 18:41:05 -0300

    Add files via upload

---
+++ Jogador Java/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Instanciar Pessoa1 = new Instanciar("Grande", "alto", "Dida", "perna predominante = destro", "perna não predominante");
+        Instanciar Pessoa2 = new Instanciar("Grande", "alto", "Ronaldo", "perna não predominante", "perna predominante = canhoto");
+        Instanciar Pessoa3 = new Instanciar("Pequena", "baixo", "Juninho", "perna predominante = destro", "perna não predominante");
+
+        System.out.println("Jogador 1: \n" +
+                            "Cabeça: " + Pessoa1.getCabeca() + "\n" +
+                            "Tronco: " + Pessoa1.getTronco() + "\n" +
+                            "Nome: " + Pessoa1.getNome() + "\n" +
+                            "Perna Direita: " + Pessoa1.getPernaDireita() + "\n" +
+                            "Perna Esquerda: " + Pessoa1.getPernaEsquerda());
+        
+        System.out.println("Jogador 2: \n" +
+                            "Cabeça: " + Pessoa2.getCabeca() + "\n" +
+                            "Tronco: " + Pessoa2.getTronco() + "\n" +
+                            "Nome: " + Pessoa2.getNome() + "\n" +
+                            "Perna Direita: " + Pessoa2.getPernaDireita() + "\n" +
+                            "Perna Esquerda: " + Pessoa2.getPernaEsquerda());
+        
+        System.out.println("Jogador 3: \n" +
+                            "Cabeça: " + Pessoa3.getCabeca() + "\n" +
+                            "Tronco: " + Pessoa3.getTronco() + "\n" +
+                            "Nome: " + Pessoa3.getNome() + "\n" +
+                            "Perna Direita: " + Pessoa3.getPernaDireita() + "\n" +
+                            "Perna Esquerda: " + Pessoa3.getPernaEsquerda());
+    }
+}
